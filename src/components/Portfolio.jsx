aa9aeeffc2ea1d1c9991cd4a400de62f175e4f34
Milton <miltoncoronel2004@gmail.com>
2023-10-21 18:21:55 -0300

    Quite boton demo

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -11,35 +11,30 @@ const Portfolio = () => {
       id: 1,
       src: ahorareconquista,
       title: "Ahora Reconquista - News Page",
-      demoLink: 'https://ahorareconquista.com.ar/', 
       codeLink: 'https://github.com/MiltonCoronel2004/ahorareconquista-newspage', 
     },
     {
       id: 2,
       src: sivige,
       title: "Sistema de Víctimas de Violencia de Género",
-      demoLink: 'https://sivige.000webhostapp.com/',
       codeLink: 'https://github.com/MiltonCoronel2004/SIVIGE',
     },
     {
       id: 3,
       src: biblioteca,
       title: "Sistema de Administración de Bibliotecas",
-      demoLink: 'https://biblioteca-escuela-nro-462.000webhostapp.com/',
       codeLink: 'https://github.com/MiltonCoronel2004/school-library-administrator',
     },
     {
       id: 4,
       src: ImgReactSearch,
       title: "React Search Images By AI",
-      demoLink: 'https://github.com/MiltonCoronel2004/React-Img-Search-API',
       codeLink: 'https://github.com/MiltonCoronel2004/React-Img-Search-API',
     },
     {
       id: 5,
       src: crud,
       title: "Sistema de Gestion de Stock",
-      demoLink: 'https://github.com/MiltonCoronel2004/CRUD-Laravel-React',
       codeLink: 'https://github.com/MiltonCoronel2004/CRUD-Laravel-React',
     },
   ];
@@ -64,9 +59,7 @@ const Portfolio = () => {
               <img src={src} alt="Work" className='rounded-md duration-200 hover:scale-105 h-48' />
               <p className='flex justify-center pt-2 text-sm text-center'>{title}</p>
               <div className='flex items-center justify-center'>
-                <a href={demoLink} target="_blank" rel="noopener noreferrer">
-                  <button onClick={onClick} className='w-1/2 px-6 m-4 duration-200 hover:scale-105'>Demo</button>
-                </a>
+
                 <a href={codeLink} target="_blank" rel="noopener noreferrer">
                   <button className='w-1/2 px-6 m-4 duration-200 hover:scale-105'>Code</button>
                 </a>
